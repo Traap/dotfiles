a800b17548da11a197b629dd733eb9b5a1aa404a
Traap <gary.a.howard@mac.com>
2021-03-16 09:34:51 -0500

    Refernce dotfiles vs ssh folder.

--- setup/msys/mklinks.bat
+++ setup/msys/mklinks.bat
@@ -1,23 +1,26 @@
-#mklink    c:\Users\%USERNAME%\.bash_logout        c:\Users\%USERNAME%\git\dotfiles\bash_logout
-#mklink    c:\Users\%USERNAME%\.bash_profile       c:\Users\%USERNAME%\git\dotfiles\bashrc
-mklink    c:\Users\%USERNAME%\.bashrc             c:\Users\%USERNAME%\git\dotfiles\bashrc 
-mklink    c:\Users\%USERNAME%\.config.vim         c:\Users\%USERNAME%\git\ssh\config.vim 
-mklink    c:\Users\%USERNAME%\.dircolors          c:\Users\%USERNAME%\git\dotfiles\dircolors 
-mklink    c:\Users\%USERNAME%\.gitconfig          c:\Users\%USERNAME%\git\ssh\gitconfig
-mklink    c:\Users\%USERNAME%\.gitignore_global   c:\Users\%USERNAME%\git\ssh\gitignore_global 
-mklink    c:\Users\%USERNAME%\.inputrc            c:\Users\%USERNAME%\git\dotfiles\inputrc 
-mklink    c:\Users\%USERNAME%\.minttyrc           c:\Users\%USERNAME%\git\dotfiles\minttyrc 
-rem # mklink    c:\Users\%USERNAME%\.mutt               c:\Users\%USERNAME%\git\mutt-office365 
-rem # mklink    c:\Users\%USERNAME%\.muttrc             c:\Users\%USERNAME%\git\mutt-office365\muttrc 
-rem # mklink    c:\Users\%USERNAME%\.tmux.conf          c:\Users\%USERNAME%\git\tmux\tmux.conf 
-mklink /D c:\Users\%USERNAME%\.vim                c:\Users\%USERNAME%\git\vim
-mklink    c:\Users\%USERNAME%\.viminfo            c:\Users\%USERNAME%\git\vim\viminfo
-mklink    c:\Users\%USERNAME%\.vimrc              c:\Users\%USERNAME%\git\vim\vimrc
-mklink    c:\Users\%USERNAME%\.vimrc_background   c:\Users\%USERNAME%\git\vim\vimrc_background
-mklink /D c:\Users\%USERNAME%\.ssh                c:\Users\%USERNAME%\git\ssh 
-mklink    c:\Users\%USERNAME%\AppData\Local\nvim\init.vim c:\Users\%USERNAME%\git\vim\init.vim
-rem # mklink /D c:\Users\%USERNAME%\.tmux               c:\Users\%USERNAME%\git\tmux 
+echo on
 
+rem mklink    c:\Users\%USERNAME%\.bash_logout        c:\Users\%USERNAME%\git\dotfiles\bash_logout
+rem mklink    c:\Users\%USERNAME%\.bash_profile       c:\Users\%USERNAME%\git\dotfiles\bashrc
+rem mklink    c:\Users\%USERNAME%\.bashrc             c:\Users\%USERNAME%\git\dotfiles\bashrc
+mklink    c:\Users\%USERNAME%\.bashrc-personal    c:\Users\%USERNAME%\git\dotfiles\bashrc-personal
+rem mklink    c:\Users\%USERNAME%\.config.vim         c:\Users\%USERNAME%\git\ssh\config.vim
+rem mklink    c:\Users\%USERNAME%\.dircolors          c:\Users\%USERNAME%\git\dotfiles\dircolors
+mklink    c:\Users\%USERNAME%\.gitconfig          c:\Users\%USERNAME%\git\dotfiles\gitconfig
+mklink    c:\Users\%USERNAME%\.gitignore_global   c:\Users\%USERNAME%\git\dotfiles\gitignore_global
+rem mklink    c:\Users\%USERNAME%\.inputrc            c:\Users\%USERNAME%\git\dotfiles\inputrc
+rem mklink    c:\Users\%USERNAME%\.minttyrc           c:\Users\%USERNAME%\git\dotfiles\minttyrc
+rem mklink    c:\Users\%USERNAME%\.mutt               c:\Users\%USERNAME%\git\mutt-office365
+rem mklink    c:\Users\%USERNAME%\.muttrc             c:\Users\%USERNAME%\git\mutt-office365\muttrc
+rem mklink    c:\Users\%USERNAME%\.tmux.conf          c:\Users\%USERNAME%\git\tmux\tmux.conf
+rem mklink    c:\Users\%USERNAME%\.viminfo            c:\Users\%USERNAME%\git\vim\viminfo
+rem mklink    c:\Users\%USERNAME%\.vimrc              c:\Users\%USERNAME%\git\vim\vimrc
+rem mklink    c:\Users\%USERNAME%\.vimrc_background   c:\Users\%USERNAME%\git\vim\vimrc_background
+rem mklink    c:\Users\%USERNAME%\AppData\Local\nvim\init.vim c:\Users\%USERNAME%\git\vim\init.vim
+rem mklink /D c:\Users\%USERNAME%\.ssh                c:\Users\%USERNAME%\git\ssh
+rem mklink /D c:\Users\%USERNAME%\.tmux               c:\Users\%USERNAME%\git\tmux
+rem mklink /D c:\Users\%USERNAME%\.vim                c:\Users\%USERNAME%\git\vim
+rem 
 echo off
 rem # I do this to force my ssh dir to my window home directory.  I'm tired of
 rem # fighting corporate networks, vpn, and other stuff. Note: I may not have
