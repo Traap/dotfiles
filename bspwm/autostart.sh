94290516d27a44e4f6ed765907c8fc47bdb32c4a
Traap <gary.a.howard@mac.com>
2021-05-24 08:22:42 -0500

    Setup ArchLinux + WSL2 helpers.

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -7,35 +7,21 @@ function run {
   fi
 }
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+# $HOME/.config/polybar/launch.sh &
 
-$HOME/.config/polybar/launch.sh &
 
-#change your keyboard if you need it
-#setxkbmap -layout be
-
-#Some ways to set your wallpaper besides variety or nitrogen
-#feh --bg-scale ~/.config/bspwm/wall.png &
-#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#feh --randomize --bg-fill ~/Képek/*
-#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
-
-dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+# dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
-conky -c $HOME/.config/bspwm/system-overview &
+# conky -c $HOME/.config/bspwm/system-overview &
 run variety &
-run nm-applet &
-run pamac-tray &
-run xfce4-power-manager &
+# run nm-applet &
+# run pamac-tray &
+# run xfce4-power-manager &
 numlockx off &
-blueberry-tray &
+# blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
-run volumeicon &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# /usr/lib/xfce4/notifyd/xfce4-notifyd &
+#run volumeicon &
