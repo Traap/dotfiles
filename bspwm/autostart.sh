ce53448d3f8437e907bc261e40e84c07c3407bca
Traap <gary.a.howard@mac.com>
2021-05-29 12:51:47 -0500

    Restore bspwm.

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -7,21 +7,21 @@ function run {
   fi
 }
 
-# $HOME/.config/polybar/launch.sh &
+$HOME/.config/polybar/launch.sh &
 
 
-# dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
-# conky -c $HOME/.config/bspwm/system-overview &
+conky -c $HOME/.config/bspwm/system-overview &
 run variety &
-# run nm-applet &
-# run pamac-tray &
-# run xfce4-power-manager &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
 numlockx off &
-# blueberry-tray &
+blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-# /usr/lib/xfce4/notifyd/xfce4-notifyd &
-#run volumeicon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+run volumeicon &
