ad9a58634f6e9499b28a20af83a3c65aa9c3c2c7
Traap <gary.a.howard@mac.com>
2022-01-30 14:32:08 -0600

    Refactor bspwm startup.

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -14,14 +14,15 @@ dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
-conky -c $HOME/.config/bspwm/system-overview &
+run conky -c $HOME/.config/bspwm/system-overview &
 run variety &
 run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
+
 numlockx off &
 setxkbmap -option caps:swapescape
-blueberry-tray &
+run blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
