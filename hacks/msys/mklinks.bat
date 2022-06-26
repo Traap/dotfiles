1dc5012a895e57931e5787f638afb7a99774ca8d
Traap <gary.a.howard@mac.com>
2022-06-25 20:09:09 -0500

    Vim symbolic links.

--- hacks/msys/mklinks.bat
+++ hacks/msys/mklinks.bat
@@ -6,15 +6,16 @@ rem mklink    c:\Users\%USERNAME%\.bashrc             c:\Users\%USERNAME%\git\do
 rem mklink    c:\Users\%USERNAME%\.bashrc-personal    c:\Users\%USERNAME%\git\dotfiles\bashrc-personal
 rem mklink    c:\Users\%USERNAME%\.config.vim         c:\Users\%USERNAME%\git\ssh\config.vim
 rem mklink    c:\Users\%USERNAME%\.dircolors          c:\Users\%USERNAME%\git\dotfiles\dircolors
-mklink    c:\Users\%USERNAME%\.gitconfig          c:\Users\%USERNAME%\git\dotfiles\gitconfig
-mklink    c:\Users\%USERNAME%\.gitignore_global   c:\Users\%USERNAME%\git\dotfiles\gitignore_global
+rem mklink    c:\Users\%USERNAME%\.gitconfig          c:\Users\%USERNAME%\git\dotfiles\gitconfig
+rem mklink    c:\Users\%USERNAME%\.gitignore_global   c:\Users\%USERNAME%\git\dotfiles\gitignore_global
 rem mklink    c:\Users\%USERNAME%\.inputrc            c:\Users\%USERNAME%\git\dotfiles\inputrc
 rem mklink    c:\Users\%USERNAME%\.minttyrc           c:\Users\%USERNAME%\git\dotfiles\minttyrc
 rem mklink    c:\Users\%USERNAME%\.mutt               c:\Users\%USERNAME%\git\mutt-office365
 rem mklink    c:\Users\%USERNAME%\.muttrc             c:\Users\%USERNAME%\git\mutt-office365\muttrc
 rem mklink    c:\Users\%USERNAME%\.tmux.conf          c:\Users\%USERNAME%\git\tmux\tmux.conf
 rem mklink    c:\Users\%USERNAME%\.viminfo            c:\Users\%USERNAME%\git\vim\viminfo
-rem mklink    c:\Users\%USERNAME%\.vimrc              c:\Users\%USERNAME%\git\vim\vimrc
+rem mklink    c:\Users\%USERNAME%\.vim                c:\Users\%USERNAME%\git\vim\vim
+rem mklink    c:\Users\%USERNAME%\_vimrc              c:\Users\%USERNAME%\git\vim\vimrc
 rem mklink    c:\Users\%USERNAME%\.vimrc_background   c:\Users\%USERNAME%\git\vim\vimrc_background
 rem mklink    c:\Users\%USERNAME%\AppData\Local\nvim\init.vim c:\Users\%USERNAME%\git\vim\vimrc
 rem mklink /D c:\Users\%USERNAME%\.ssh                c:\Users\%USERNAME%\git\ssh
