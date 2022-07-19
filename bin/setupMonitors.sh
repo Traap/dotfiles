610304aea9bb8471ab8556cf302fbd7f0d50ecf1
Traap <gary.a.howard@mac.com>
2022-07-19 08:11:22 -0500

    bspmwm upgrades.
    1. more sxhkd keys.
    2. Swap DP-0 and DP-2
    3. Are we having fun yet?

--- bin/setupMonitors.sh
+++ bin/setupMonitors.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+setxkbmap -option caps:swapescape
+xmodmap -e "pointer = 3 2 1 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 4 5"
 xrandr \
   --output HDMI-0   --mode 2560x1440 --pos    0x0 --rotate normal \
-  --output DP-2     --mode 2560x1440 --pos 2560x0 --rotate normal \
-  --output DP-0     --mode 2560x1440 --pos 5120x0 --rotate normal \
+  --output DP-0     --mode 2560x1440 --pos 2560x0 --rotate normal \
+  --output DP-2     --mode 2560x1440 --pos 5120x0 --rotate normal \
   --output DP-1     --off  \
   --output DP-1-1   --off  \
   --output DP-3     --off  \
--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -11,11 +11,10 @@ $HOME/.config/polybar/launch.sh &
 
 
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
-xsetroot -cursor_name left_ptr &
+run xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 run conky -c $HOME/.config/bspwm/system-overview &
-run variety &
 run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
@@ -26,7 +25,10 @@ picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
 # Turn off numlock, swap Cap Lock and Escape keys, and left-handed mouse.
-run volumeicon &
-run numlockx off&
-run setxkbmap -option caps:swapescape&
+run volumeicon  &
+run numlockx off &
+run setxkbmap -option caps:swapescape &
 xmodmap -e "pointer = 3 2 1 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 4 5"
+
+#!/bin/bash
+feh --bg-scale --slideshow-delay 900 ~/.config/wallpaper/. &
