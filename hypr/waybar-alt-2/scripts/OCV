2cc8802b13dc004f7a63611ee385e56040a8b6c9
Traap <gary.a.howard@mac.com>
2024-12-21 12:52:20 -0800

    Refactor hypr folder by extracting programs.
    The following programs have been moved to their own folders:
    sylock, wlogout, foot, waybar, wayfire, wofi, and wofifull.
    
    The extraction allows me to configure and maintain these program
    independent of the Wayland or Wayfire desktop I am using.

--- hypr/waybar-alt-2/scripts/OCV
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yad --width=400 --height=200 \
---center \
---fixed \
---title="Calendar" \
---no-buttons \
-yad \
-           --calendar
--- hypr/waybar-alt-2/scripts/cleanup_after_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sleep 20
-hyprctl keyword windowrule "workspace unset,nmtui-colors"
-hyprctl keyword windowrule "workspace unset,calamares"
-hyprctl keyword windowrule "workspace unset,keyhint.sh"
-hyprctl keyword windowrule "workspace unset,firedragon"
