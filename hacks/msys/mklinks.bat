ea291f162a304ad5ad2e3de3919890d41a5a477e
Traap <gary.a.howard@mac.com>
2025-04-19 09:50:15 -0700

    Adjust to bashrc-personal rename.

--- hacks/msys/mklinks.bat
+++ hacks/msys/mklinks.bat
@@ -1,24 +1,24 @@
 rem echo OFF
 
 rem mklink    %USERPROFILE%\.bash_logout        %USERPROFILE%\git\dotfiles\bash\bash_logout
-rem mklink    %USERPROF:ILE%\.bash_profile       %USERPROFILE%\git\dotfiles\bash\bash_profile
-rem mklink    %USERPROFILE%\.bashrc             %USERPROFILE%\git\dotfiles\bash\bashrc
-rem mklink    %USERPROFILE%\.bashrc-personal    %USERPROFILE%\git\dotfiles\bash\bashrc-personal
-rem mklink    %USERPROFILE%\.config.vim         %USERPROFILE%\git\ssh\config.vim
-rem mklink    %USERPROFILE%\.dircolors          %USERPROFILE%\git\dotfiles/bash\dircolors
-rem mklink    %USERPROFILE%\.gitconfig          %USERPROFILE%\git\dotfiles\git\gitconfig
-rem mklink    %USERPROFILE%\.gitignore_global   %USERPROFILE%\git\dotfiles\git\gitignore_global
-rem mklink    %USERPROFILE%\.inputrc            %USERPROFILE%\git\dotfiles\bash\inputrc
+mklink    %USERPROFILE%\.bash_profile       %USERPROFILE%\git\dotfiles\bash\bash_profile
+mklink    %USERPROFILE%\.bashrc             %USERPROFILE%\git\dotfiles\bash\bashrc
+mklink    %USERPROFILE%\.bashrc-personal    %USERPROFILE%\git\dotfiles\bash\bashrc_personal
+mklink    %USERPROFILE%\.config.vim         %USERPROFILE%\git\ssh\config.vim
+mklink    %USERPROFILE%\.dircolors          %USERPROFILE%\git\dotfiles/bash\dircolors
+mklink    %USERPROFILE%\.gitconfig          %USERPROFILE%\git\dotfiles\git\gitconfig
+mklink    %USERPROFILE%\.gitignore_global   %USERPROFILE%\git\dotfiles\git\gitignore_global
+mklink    %USERPROFILE%\.inputrc            %USERPROFILE%\git\dotfiles\bash\inputrc
 rem mklink    %USERPROFILE%\.minttyrc           %USERPROFILE%\git\dotfiles/bash\minttyrc
 rem mklink    %USERPROFILE%\.mutt               %USERPROFILE%\git\mutt-office365
 rem mklink    %USERPROFILE%\.muttrc             %USERPROFILE%\git\mutt-office365\muttrc
-rem mklink    %USERPROFILE%\.tmux.conf          %USERPROFILE%\git\tmux\tmux.conf
-rem mklink    %USERPROFILE%\.viminfo            %USERPROFILE%\git\vim\viminfo
-rem mklink    %USERPROFILE%\_vimrc              %USERPROFILE%\git\vim\vimrc
+mklink    %USERPROFILE%\.tmux.conf          %USERPROFILE%\git\tmux\tmux.conf
+mklink    %USERPROFILE%\.viminfo            %USERPROFILE%\git\vim\viminfo
+mklink    %USERPROFILE%\_vimrc              %USERPROFILE%\git\vim\vimrc
 rem mklink    %USERPROFILE%\.vimrc_background   %USERPROFILE%\git\vim\vimrc_background
 rem mklink    %USERPROFILE%\AppData\Local\nvim\init.vim %USERPROFILE%\git\vim\vimrc
-rem mklink /D %USERPROFILE%\.ssh                %USERPROFILE%\git\ssh
-rem mklink /D %USERPROFILE%\.tmux               %USERPROFILE%\git\tmux
+mklink /D %USERPROFILE%\.ssh                %USERPROFILE%\git\ssh
+mklink /D %USERPROFILE%\.tmux               %USERPROFILE%\git\tmux
 mklink /D %USERPROFILE%\.vim                %USERPROFILE%\git\vim
 
 rem echo off
