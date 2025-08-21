4aff3fd222c6d8935cf3298ab3606145960e0138
Traap <gary.a.howard@mac.com>
2025-08-20 23:51:01 -0700

    Make a Windows usb device.

--- bash/bin/format-usb-exfat.sh
+++ bash/bin/format-usb-exfat.sh
@@ -2,7 +2,7 @@
 
 # ------------------------------------------------------------------------
 # WARNING: This script will ERASE all data on /dev/sda.
-# Make sure /dev/sda is the USB device and not your system drive.
+# Be absolutely sure that /dev/sda is your USB device and not your system drive.
 # ------------------------------------------------------------------------
 
 DEVICE="/dev/sda"
@@ -10,10 +10,8 @@ PART="${DEVICE}1"
 LABEL="FastUSB"
 MOUNTPOINT="/mnt/usb"
 
-# Confirm device with user
-echo "Are you 100% sure you want to erase $DEVICE and "
-echo "label it '$LABEL'? "
-read -rp "Type 'YES' to proceed: " confirm
+# Confirm with the user
+read -rp "Are you 100% sure you want to erase $DEVICE and label it '$LABEL'? Type 'YES' to proceed: " confirm
 if [[ "$confirm" != "YES" ]]; then
   echo "Aborted."
   exit 1
@@ -21,27 +19,38 @@ fi
 
 echo ">>> Unmounting any mounted partitions on $DEVICE..."
 sudo umount -l ${DEVICE}?* 2>/dev/null
+sudo udisksctl unmount -b ${DEVICE}?* 2>/dev/null
 
-echo ">>> Wiping filesystem signatures..."
+echo ">>> Refreshing kernel view of $DEVICE..."
+sleep 2
+sudo partprobe "$DEVICE"
+
+echo ">>> Wiping existing filesystem signatures..."
 sudo wipefs -a "$DEVICE"
 
-echo ">>> Creating GPT partition table..."
-sudo parted -s "$DEVICE" mklabel gpt
+echo ">>> Creating MBR (msdos) partition table..."
+sudo parted -s "$DEVICE" mklabel msdos
 
-echo ">>> Creating single ext4 partition..."
-sudo parted -s "$DEVICE" mkpart primary ext4 0% 100%
+echo ">>> Creating single primary partition..."
+sudo parted -s "$DEVICE" mkpart primary 0% 100%
 
-# Wait for kernel to register new partition
-sleep 2
+# Wait for /dev/sda1 to appear
+sleep 3
+sudo partprobe "$DEVICE"
 
-echo ">>> Formatting $PART as ext4 with label '$LABEL'..."
-sudo mkfs.exfat -L "$LABEL" "$PART"
+if [ ! -b "$PART" ]; then
+  echo ">>> ERROR: Partition $PART not found. Aborting."
+  exit 1
+fi
 
-echo ">>> Creating mount point: $MOUNTPOINT"
+echo ">>> Formatting $PART as exFAT with label '$LABEL'..."
+sudo mkfs.exfat -n "$LABEL" "$PART"
+
+echo ">>> Creating mount point at $MOUNTPOINT..."
 sudo mkdir -p "$MOUNTPOINT"
 
-echo ">>> Mounting $PART to $MOUNTPOINT"
+echo ">>> Mounting $PART to $MOUNTPOINT..."
 sudo mount "$PART" "$MOUNTPOINT"
 
-echo ">>> Done. USB is mounted at $MOUNTPOINT with label '$LABEL'"
+echo ">>> Success! USB drive is mounted at $MOUNTPOINT and labeled '$LABEL'"
 
